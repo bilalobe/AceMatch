b1f9ab33c568dc808ed7511ba8e584e06440c6f6
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-03-19 10:10:28 +0000

    Refactor GestionScore class: remove redundant database parameter from methods, enhance signal declarations, and improve class structure

--- app/data/mgmt/GestionScores.h
+++ app/data/mgmt/GestionScores.h
@@ -4,19 +4,32 @@
 #include <QString>
 #include <QList>
 #include <QSqlDatabase>
+#include <QObject>
 #include "..\Score.h"
 
-class GestionScore
+class GestionScore : public QObject
 {
+    Q_OBJECT
+
 public:
     GestionScore(const QSqlDatabase& db);
     ~GestionScore();
 
-    bool ajouterScore(const QSqlDatabase& db, int matchId, int score1, int score2);
-    bool supprimerScore(const QSqlDatabase& db, int scoreId); 
-    bool modifierScore(const QSqlDatabase& db, int scoreId, int newScore1, int newScore2);
-    QList<Score> getScores(const QSqlDatabase& db) const; 
-    Score getScoreById(const QSqlDatabase& db, int scoreId) const; 
+    bool ajouterScore(int matchId, int score1, int score2);
+    bool supprimerScore(int scoreId); 
+    bool modifierScore(int scoreId, int newScore1, int newScore2);
+    QList<Score> getScores() const;
+    Score getScoreById(int scoreId) const;
+    QList<Score> searchScores(const QString& searchTerm) const;
+
+signals:
+    void scoreAdded(int id, int matchId, int score1, int score2);
+    void scoreDeleted(int id);
+    void scoreUpdated(int id, int newScore1, int newScore2);
+    void scoreFound(int id, int matchId, int score1, int score2);
+
+private:
+    QSqlDatabase db;
 };
 
 #endif // GESTIONSCORE_H
