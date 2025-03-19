7ed264867f63020e5642ad1635d24221bc77b4c0
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-03-19 10:10:34 +0000

    Refactor GestionTerrains class: remove redundant database parameter from methods, enhance signal declarations, and improve class structure

--- app/data/mgmt/GestionTerrains.h
+++ app/data/mgmt/GestionTerrains.h
@@ -4,18 +4,32 @@
 #include <QString>
 #include <QList>
 #include <QSqlDatabase>
+#include <QObject>
 #include "..\Terrain.h"
 
-class GestionTerrains
+class GestionTerrains : public QObject
 {
+    Q_OBJECT
+
 public:
     GestionTerrains(const QSqlDatabase& db);
     ~GestionTerrains();
 
-    bool ajouterTerrain(const QSqlDatabase& db, const QString& nom, const QString& type);
-    bool supprimerTerrain(const QSqlDatabase& db, int terrainId); 
-    bool modifierTerrain(const QSqlDatabase& db, int terrainId, const QString& newName, const QString& newType);
-    QList<Terrain> getTerrains(const QSqlDatabase& db) const; 
+    bool ajouterTerrain(const QString& nom, const QString& type);
+    bool supprimerTerrain(int terrainId); 
+    bool modifierTerrain(int terrainId, const QString& newName, const QString& newType);
+    QList<Terrain> getTerrains() const;
+    Terrain getTerrainById(int terrainId) const;
+    QList<Terrain> searchTerrains(const QString& searchTerm) const;
+
+signals:
+    void terrainAdded(int id, const QString& nom, const QString& type);
+    void terrainUpdated(int id, const QString& nom, const QString& type);
+    void terrainDeleted(int id);
+    void terrainFound(int id, const QString& nom, const QString& type);
+
+private:
+    QSqlDatabase db;
 };
 
 #endif // GESTIONTERRAINS_H
