4f562f5ff7cc976ee55e1d2134fef47307a9331a
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-03-19 10:09:03 +0000

    Refactor GestionJoueurs class: remove redundant database parameter from methods and add signal declarations

--- app/data/mgmt/GestionJoueurs.h
+++ app/data/mgmt/GestionJoueurs.h
@@ -5,19 +5,33 @@
 #include <QString>
 #include <QList>
 #include <QSqlDatabase>
+#include <QObject>
 #include "..\Joueur.h"
 
-class GestionJoueurs
+class GestionJoueurs : public QObject
 {
+    Q_OBJECT
+
 public:
     GestionJoueurs(const QSqlDatabase& db);
     ~GestionJoueurs();
 
-    bool ajouterJoueur(const QSqlDatabase& db, const QString& nom, int rang);
-    bool supprimerJoueur(const QSqlDatabase& db, const QString& nom);
-    bool modifierJoueur(const QSqlDatabase& db, const QString& nom, int nouveauRang);
-    QList<Joueur> getJoueurs(const QSqlDatabase& db) const; 
-    Joueur getJoueurByName(const QSqlDatabase& db, const QString& name) const; 
+    bool ajouterJoueur(const QString& nom, int rang);
+    bool supprimerJoueur(const QString& nom);
+    bool modifierJoueur(const QString& nom, int nouveauRang);
+    QList<Joueur> getJoueurs() const;
+    Joueur getJoueurByName(const QString& name) const;
+    QList<Joueur> searchJoueurs(const QString& searchTerm) const;
+    Match getMatchById(int matchId) const;
+
+signals:
+    void joueurAdded(const QString& nom, int rang);
+    void joueurDeleted(const QString& nom);
+    void joueurUpdated(const QString& nom, int nouveauRang);
+    void joueurFound(const QString& nom, int rang);
+
+private:
+    QSqlDatabase db;
 };
 
 #endif // GESTIONJOUEURS_H
